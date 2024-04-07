e5b43bcd1c018f0a4476e788f5aaff78a8c2b0ba
sarwaryousaf <165546261+sarwaryousaf@users.noreply.github.com>
2024-04-07 15:31:39 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const num= Math.floor(Math.random()* 6+1);
+const result = await inquirer.prompt([{
+name:"userGuessNumber",
+type:"number",
+message:"Please Guess any Number from 1 to 6",
+},])
+
+if(result.userGuessNumber===num)
+{console.log("You Guess a Right Number.");
+}
+else
+{
+    console.log("You Guess a Wrong Number.")
+}
+console.log(num)
